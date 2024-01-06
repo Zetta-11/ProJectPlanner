e65b9635d73dcd6efbf382e96a09a079e3147dac
Klim Menkov <klim.menkov@ukr.net>
2024-01-06 18:27:41 +0200

    Initial commit

---
+++ src/main/java/com/dnu/klimmenkov/projectplanner/ProJectPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.dnu.klimmenkov.projectplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProJectPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProJectPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dnu/klimmenkov/projectplanner/ProJectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnu.klimmenkov.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProJectPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
