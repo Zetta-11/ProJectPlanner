858e25a2cbcb5f0cd709dcb7c5af605c19757421
Klim Menkov <klim.menkov@ukr.net>
2024-01-23 00:16:57 +0200

    FkyWay migration file was updated.

--- src/main/resources/static/db/migration/V1__create_table_users.sql
+++ src/main/resources/static/db/migration/V1__create_table_users.sql
@@ -1,8 +1,27 @@
+CREATE TABLE products
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    description  TEXT,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE projects
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    project_name VARCHAR(100) NOT NULL,
+    product_id   INT,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES products (id)
+);
+
 CREATE TABLE users
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
-    username   VARCHAR(50) UNIQUE NOT NULL,
+    login      VARCHAR(50) UNIQUE NOT NULL,
     password   VARCHAR(255)       NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    roles      VARCHAR(20)        NOT NULL
+    roles      VARCHAR(20)        NOT NULL,
+    project_id INT,
+    FOREIGN KEY (project_id) REFERENCES projects (id)
 );
