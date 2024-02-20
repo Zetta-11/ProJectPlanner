9ff8f62399ffeff2065d936f6e6fc1f1c9993f94
Klim Menkov <klim.menkov@ukr.net>
2024-02-20 22:34:15 +0200

    All projects page was implemented

--- src/main/java/com/dnu/klimmenkov/projectplanner/controller/HomeController.java
+++ src/main/java/com/dnu/klimmenkov/projectplanner/controller/HomeController.java
@@ -1,34 +1,51 @@
 package com.dnu.klimmenkov.projectplanner.controller;
 
+import com.dnu.klimmenkov.projectplanner.entity.Project;
 import com.dnu.klimmenkov.projectplanner.entity.User;
+import com.dnu.klimmenkov.projectplanner.service.ProjectService;
 import com.dnu.klimmenkov.projectplanner.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
 
-    @Autowired
-    private UserService userService;
+    final private UserService userService;
+
+    final private ProjectService projectService;
 
     @GetMapping("/home")
-    public String home() {
+    public String getHomePage() {
         return "home/index";
     }
 
     @GetMapping("/about")
-//    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String unsecured() {
+    public String getAboutPage() {
         return "home/about";
     }
 
-    //TEST METHOD REMOVE WHEN PROJECT IS DONE
+    @GetMapping("/projects")
+    public String getProjectsPage(Model model) {
+        model.addAttribute("projects", projectService.getAllProjects());
+        return "home/allProjects";
+    }
+
+    @GetMapping("/projects/{projectId}")
+    public String showProjectDetails(@PathVariable("projectId") int projectId, Model model) {
+        Project project = projectService.getProjectById(projectId);
+        if (project == null) {
+            return "home/allProjects";
+        }
+        model.addAttribute("project", project);
+        return "home/projectDetails";
+    }
+
+    //TODO TEST METHOD REMOVE WHEN PROJECT IS DONE
     @PostMapping("/new-user")
     @ResponseBody
     public String createNewUser(@RequestBody User user) {
