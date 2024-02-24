c68737f8d1fb26768d787ac66167902053085ff3
Klim Menkov <klim.menkov@ukr.net>
2024-02-24 15:49:44 +0200

    AddTask page was created without back-end.

--- src/main/java/com/dnu/klimmenkov/projectplanner/controller/TasksController.java
+++ src/main/java/com/dnu/klimmenkov/projectplanner/controller/TasksController.java
@@ -2,14 +2,20 @@ package com.dnu.klimmenkov.projectplanner.controller;
 
 import com.dnu.klimmenkov.projectplanner.entity.Project;
 import com.dnu.klimmenkov.projectplanner.entity.Task;
+import com.dnu.klimmenkov.projectplanner.entity.User;
+import com.dnu.klimmenkov.projectplanner.service.ProjectService;
 import com.dnu.klimmenkov.projectplanner.service.TaskService;
 import com.dnu.klimmenkov.projectplanner.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,6 +27,7 @@ public class TasksController {
 
     private final TaskService taskService;
     private final UserService userService;
+    private final ProjectService projectService;
 
     @GetMapping()
     public String getTasksPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
@@ -40,4 +47,25 @@ public class TasksController {
         return "home/tasks";
     }
 
+    @GetMapping("/new")
+    public String getTasksCreatePage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        List<User> users = userService.getAllUsers();
+        List<Project> projects = projectService.getAllProjects();
+
+        model.addAttribute("task", new Task());
+        model.addAttribute("users", users);
+        model.addAttribute("projects", projects);
+
+        return "home/addTask";
+    }
+
+    @PostMapping()
+    public String createNewTask(@Valid @ModelAttribute("task") Task task, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            return "home/addTask";
+        }
+
+        // Add logic to save task
+        return "redirect:/tasks";
+    }
 }
